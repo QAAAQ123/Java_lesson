3579fd2537b6379e78d515c90bd20a9455c33794
김재원 <wodnjs9058@naver.com>
2023-08-05 14:59:58 +0900

    third fix

---
+++ src/MathBeanTest.java
@@ -0,0 +1,13 @@
+public class MathBeanTest {
+    public static void main(String[] args){
+        MathBean math = new MathBean(); //heap area
+
+        math.printClassName();
+        math.printNumber(5000);
+        int x = math.getOne();
+        System.out.println(x);
+        int value = math.plus(200,300);
+        System.out.println(value);
+        
+    }
+}
--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -1,4 +1,8 @@
 public class VendingMachine {
+    public static void printVersion(){
+        System.out.println("Version 1.0");
+    }
+
     public String pushProductButton(int menuId){
         System.out.println(menuId + "를 전달 받았습니다.");
         return "콜라";
